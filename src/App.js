5928a74f9cbd1d2404177b73e8d6d9a60d55ecdb
Charlie Albert <charlieraph36@gmail.com>
2022-05-23 18:58:59 +0300

    added counter file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Greet from './Components/Greet';
 import Welcome from './Components/Welcome';
 import Hello from './Components/Hello';
 import Message from './Components/Message';
+import Counter from './Components/Counter';
 
 function App() {
   return (
     <div className="App">
+    <Counter />
     {/*
+    <Message />
     <Greet name="Charlie" heroName="Albert">
         <button>Action</button>
       </Greet>
@@ -17,7 +20,6 @@ function App() {
       <Welcome name="Jessica Pearson"/>
       <Welcome name="Harvey Specter"/>
     */}
-      <Message />
       {/*<Hello /> */}
     </div>
   );
